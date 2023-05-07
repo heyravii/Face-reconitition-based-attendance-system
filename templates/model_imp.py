a3c9ec640728c737f73fc98d9c58b6928b958f9f
heyravii <lamichhaneravi1@gmail.com>
2023-05-08 01:46:53 +0545

    final commit

---
+++ templates/model_imp.py
@@ -0,0 +1,27 @@
+
+import numpy as np
+import cv2
+import os
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import joblib
+
+
+def identify_face(facearray):
+    model = joblib.load('static/face_recognition_model.pkl')
+    return model.predict(facearray)
+
+def train_model():
+    faces = []
+    labels = []
+    userlist = os.listdir('static/faces')
+    for user in userlist:
+        for imgname in os.listdir(f'static/faces/{user}'):
+            img = cv2.imread(f'static/faces/{user}/{imgname}')
+            resized_face = cv2.resize(img, (50, 50))
+            faces.append(resized_face.ravel())
+            labels.append(user)
+    faces = np.array(faces)
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(faces,labels)
+    joblib.dump(knn,'static/face_recognition_model.pkl')
